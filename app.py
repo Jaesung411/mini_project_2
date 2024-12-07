c3a961ae84426ffb4bba4be5dc3ffba01cec88b3
jaesung <dlawo411@gmail.com>
2024-12-07 13:23:14 +0900

    make health check function

--- app.py
+++ app.py
@@ -14,5 +14,9 @@ app.register_blueprint(photo_detail_bp)
 def welcome():
     return render_template('login.html')
 
+@app.route('/health')
+def health_check():
+    return "Healthy", 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
